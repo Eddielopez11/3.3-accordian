e209d8827627f678509af7074d565cca3a25bffe
Eduardo Lopez <eddielopez2101@gmail.com>
2016-08-17 17:06:11 -0500

    everything is working, now needs more styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,5 +4,20 @@ document.addEventListener("DOMContentLoaded", function(){
 
   var $accordionElement = $("[data-js='accordion']")
 
+  // $accordionElement.on("click", "[data-js='accordion']", function(e){
+  //   var $accordionClicked = $(e.currentTarget);
+  //   $accordionClicked.toggleClass('clicked');
+  //
+  // });
+
+  $(function() {
+    $("#accordion").find(".accordion__toggle").click(function(){
+
+      $(this).next().slideToggle("fast");
+
+      $(".accordion__pannel").not($(this).next()).slideUp("fast");
+
+    });
+  });
 
 });
