a316a3f7ea806bae3c9d0b96f715bc914d75bd61
Eduardo Lopez <eddielopez2101@gmail.com>
2016-08-17 20:25:22 -0500

    added comments to remember what code does

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,20 +2,21 @@
 
 document.addEventListener("DOMContentLoaded", function(){
 
-  var $accordionElement = $("[data-js='accordion']")
-
-  // $accordionElement.on("click", "[data-js='accordion']", function(e){
-  //   var $accordionClicked = $(e.currentTarget);
-  //   $accordionClicked.toggleClass('clicked');
-  //
-  // });
+// jquery makes it a little too easy without knowing how to do much in
+// regular vanilla javascript. i suppose thats why employers now want you to
+// really know vanilla. i'd love to be able to do vanilla this easy one day.
 
   $(function() {
+    // locates id accordion which is a div and looks for the following
+    // which is set to accordion toggle, linked in to my headers
+    // and on click performs the following funcion.
     $("#accordion").find(".accordion__toggle").click(function(){
-
-      $(this).next().slideToggle("fast");
-
-      $(".accordion__pannel").not($(this).next()).slideUp("fast");
+      // headers sibling(this -> next) toggles the slide or show which
+      // preset in jquery in a slow speed
+      $(this).next().slideToggle("slow");
+      // if the header is not the one being clicked on,
+      // then the sibling following that header will slide up, or hide
+      $(".accordion__pannel").not($(this).next()).slideUp("slow");
 
     });
   });
